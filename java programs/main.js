0d0a8bb6fd6c380e0964544b5ec303971881ef0c
shruthi <shruthirs5963@gmail.com>
2023-04-13 11:53:41 +0530

    okiii

---
+++ java programs/main.js
@@ -0,0 +1,50 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+
+function getItems(){
+    const value = localStorage.getItem("todo-test") || "[]";
+
+return JSON.parse(value);
+}
+
+function setItems(items){
+    const itemsJson= JASON.stringify(items);
+
+    localStorage.setItem("todo-test",itemsJson);
+
+}
+function addItem(){
+    items.unshift({
+        description:"", 
+        completed: false
+
+    });
+    setItems(items);
+    refreshList();
+}
+
+function refreshList(){
+    //TODO: SORT items
+
+    ITEMS_CONTAINER.innerHTML="";
+
+    for(const item of items) {
+        const itemElement = ITEM_TEMPLATE.content.cloneNode(True);
+        const descriptionInput= itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        ITEMS_CONTAINER.append(itemElement);
+    }
+}
+ADD_BUTTON.addEventListener("click", () => {
+    addItem();
+
+});
+
+refreshList();
